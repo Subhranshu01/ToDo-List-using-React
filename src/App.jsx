e498dddcd05747313f67504033ab83b85ba49381
subhranshu01 <subhranshumoharana999@gmail.com>
2024-07-23 12:16:08 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -7,30 +7,42 @@ function App() {
     event.preventDefault();
     setTitle("");
     setDescription("");
-    setMainTasks([...mainTasks, { title: title, description: description }]);
+    setMainTasks([...mainTasks, { title: title, description: description,iscomplete:"complete" }]);
     console.log(mainTasks);
   };
 const deletehandler=(index)=>{
   
    mainTasks.splice(index,1)
    setMainTasks([...mainTasks])
-   
-
 }
+const completehander=(index)=>{
+  mainTasks[index].iscomplete="completed"
+  setMainTasks([...mainTasks])
+  setIsDisabled(true)
+}
+
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [mainTasks, setMainTasks] = useState([]);
+ const [isDisabled, setIsDisabled] = useState(false);
+//  const [label, setLabel] = useState('Complete');
   let renderTask = <h5 className="text-gray-500">No tasks yet</h5>;
   if (mainTasks.length > 0) {
    renderTask = mainTasks.map((task, index) => (
-      <div key={index} className="flex justify-between p-4 bg-white rounded-lg shadow-md my-2">
+      <div key={index}  className={` flex justify-between p-4 rounded-lg shadow-md my-2 ${mainTasks[index].iscomplete==="completed" ? 'bg-slate-300' : 'bg-white'}`}>
         <div>
           <h4 className="text-lg font-semibold text-gray-800">{task.title}</h4>
           <h5 className="text-gray-600">{task.description}</h5>
         </div>
-        <button className="text-white-700 p-2 px-3 rounded-md h-fit bg-red-400 hover:font-bold hover:bg-red-600" onClick={()=>{
+        <div className="flex flex-row gap-2"> <button className="text-white-700 p-2 px-3 rounded-md h-fit bg-red-400 hover:font-bold hover:bg-red-600" onClick={()=>{
           deletehandler(index)
-        }}>delete</button>
+        }} >delete</button>
+       
+        <button className="text-white-700 p-2 px-3 rounded-md h-fit bg-green-200 hover:font-bold hover:bg-green-600" onClick={()=>{
+          completehander(index)
+        }}  >{mainTasks[index].iscomplete}</button> 
+        </div>
+       
       </div>
     ));
   }
